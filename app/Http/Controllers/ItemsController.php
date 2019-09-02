4070e8e053cbb61241e4afd927343d3434e802c6
uni51 <uni51@users.noreply.github.com>
2019-09-03 05:39:58 +0900

    rakuten api

---
+++ app/Http/Controllers/ItemsController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ItemsController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $keyword = request()->keyword;
+        $items = [];
+        if ($keyword) {
+            $client = new \RakutenRws_Client();
+            $client->setApplicationId(env('RAKUTEN_APPLICATION_ID'));
+
+            $rws_response = $client->execute('IchibaItemSearch', [
+                'keyword' => $keyword,
+                'imageFlag' => 1,
+                'hits' => 20,
+            ]);
+
+            // 扱い易いように Item としてインスタンスを作成する（保存はしない）
+            foreach ($rws_response->getData()['Items'] as $rws_item) {
+                $item = new \App\Item();
+                $item->code = $rws_item['Item']['itemCode'];
+                $item->name = $rws_item['Item']['itemName'];
+                $item->url = $rws_item['Item']['itemUrl'];
+                $item->image_url = str_replace('?_ex=128x128', '', $rws_item['Item']['mediumImageUrls'][0]['imageUrl']);
+                $items[] = $item;
+            }
+        }
+
+        return view('items.create', [
+            'keyword' => $keyword,
+            'items' => $items,
+        ]);
+    }
+
+}
